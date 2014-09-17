44b534efcee0d6d8d6d73e7b9bf316344b710d22
mxro <mxro@nowhere.com>
2014-09-18 09:12:32 +1000

    Reworking embedded server API Autocommit Thu Sep 18 09:12:32 EST 2014

--- src/main/java/one/utils/jre/concurrent/ConcurrencyJre.java
+++ src/main/java/one/utils/jre/concurrent/ConcurrencyJre.java
@@ -1,9 +1,17 @@
 package one.utils.jre.concurrent;
 
+import one.utils.jre.concurrent.internal.JreConcurrencyFactory;
+import de.mxro.concurrency.Concurrency;
+import de.mxro.factories.Factory;
+
 public class ConcurrencyJre {
 
-    public static Factory<?, ?, ?> createFactory() {
+    public static Concurrency create() {
+        return new JreConcurrency();
+    }
 
+    public static Factory<?, ?, ?> createFactory() {
+        return new JreConcurrencyFactory();
     }
 
 }
