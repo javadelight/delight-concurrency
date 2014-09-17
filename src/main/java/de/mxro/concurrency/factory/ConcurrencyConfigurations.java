d6e5df9415447a5f3679ed8eb38ee102a885b427
mxro <mxro@nowhere.com>
2014-09-18 09:18:20 +1000

    a

---
+++ src/main/java/de/mxro/concurrency/factory/ConcurrencyConfigurations.java
@@ -0,0 +1,10 @@
+package de.mxro.concurrency.factory;
+
+public class ConcurrencyConfigurations {
+
+    public static ConcurrencyConfiguration any() {
+        return new ConcurrencyConfiguration() {
+        };
+    }
+
+}
