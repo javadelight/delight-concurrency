1ac91a4feb94d78195e3460ac6c4b54c2b9d80ac
mxro <mxro@nowhere.com>
2015-07-13 09:59:45 +1000

    Implementation overlay database Autocommit Mon Jul 13 09:59:45 EST 2015

--- src/main/java/delight/concurrency/schedule/SequentialOperationScheduler.java
+++ src/main/java/delight/concurrency/schedule/SequentialOperationScheduler.java
@@ -113,7 +113,7 @@ public class SequentialOperationScheduler {
 
                 synchronized (scheduled) {
 
-                    entry = scheduled.pop();
+                    entry = scheduled.pollFirst();
                 }
 
             } else {
@@ -129,7 +129,7 @@ public class SequentialOperationScheduler {
                 }
                 synchronized (scheduled) {
 
-                    entry = scheduled.removeFirst();
+                    entry = scheduled.pollFirst();
                 }
             }
         }
