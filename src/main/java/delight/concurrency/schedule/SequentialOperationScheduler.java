9ccf6e53023957b7473255c1cdfb6cd406a3a3a2
mxro <mxro@nowhere.com>
2015-09-23 08:27:18 +1000

    Develop SQL server implementation Autocommit Wed Sep 23 08:27:18 EST 2015

--- src/main/java/delight/concurrency/schedule/SequentialOperationScheduler.java
+++ src/main/java/delight/concurrency/schedule/SequentialOperationScheduler.java
@@ -15,7 +15,7 @@ import java.util.LinkedList;
 
 public class SequentialOperationScheduler {
 
-    private static final boolean ENABLE_LOG = false;
+    private static final boolean ENABLE_LOG = true;
 
     private final LinkedList<OperationEntry<Object>> scheduled;
     private final SimpleExecutor executor;
