432f846a3934ba81117664ff6eb6680293ce78df
mxro <mxro@nowhere.com>
2015-09-24 12:12:06 +1000

    Develop SQL server implementation Autocommit Thu Sep 24 12:12:06 EST 2015

--- src/main/java/delight/concurrency/schedule/SequentialOperationScheduler.java
+++ src/main/java/delight/concurrency/schedule/SequentialOperationScheduler.java
@@ -295,6 +295,27 @@ public class SequentialOperationScheduler {
             }
         });
 
+        ops.add(new Operation<Success>() {
+
+            @Override
+            public void apply(final ValueCallback<Success> callback) {
+                executorForTimeouts.shutdown(new WhenExecutorShutDown() {
+
+                    @Override
+                    public void onSuccess() {
+
+                        callback.onSuccess(Success.INSTANCE);
+                    }
+
+                    @Override
+                    public void onFailure(final Throwable t) {
+                        callback.onFailure(t);
+                    }
+                });
+            }
+
+        });
+
         AsyncCommon.sequential(ops, AsyncCommon.embed(shutdownCallback.get(), new Closure<List<Success>>() {
 
             @Override
