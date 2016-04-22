3239fcf16bc910c704bd675b0066abcbd08e778e
mxro <mxro@nowhere.com>
2016-04-22 12:24:29 +1000

    Improving ObjectHub Build Environment. Autocommit Fri Apr 22 12:24:29 AEST 2016

--- src/test/java/delight/concurrency/tests/TestSequentialSchedulerError.java
+++ src/test/java/delight/concurrency/tests/TestSequentialSchedulerError.java
@@ -1,10 +1,12 @@
 package delight.concurrency.tests;
 
+import delight.async.AsyncCommon;
 import delight.async.Operation;
 import delight.async.callbacks.ValueCallback;
 import delight.async.jre.Async;
 import delight.concurrency.jre.ConcurrencyJre;
 import delight.concurrency.schedule.SequentialOperationScheduler;
+import delight.functional.Closure;
 import delight.functional.Success;
 
 import org.junit.Assert;
@@ -44,6 +46,72 @@ public class TestSequentialSchedulerError {
         } catch (final Throwable t) {
             // as expected
         }
+
+        Async.waitFor(new Operation<Success>() {
+
+            @Override
+            public void apply(final ValueCallback<Success> callback) {
+
+                scheduler.schedule(new Operation<Success>() {
+
+                    @Override
+                    public void apply(final ValueCallback<Success> callback) {
+                        callback.onSuccess(Success.INSTANCE);
+                    }
+
+                }, callback);
+
+            }
+
+        });
+
+    }
+
+    /**
+     * Test that even if an error occurs, further operations can be submitted to
+     * the scheduler.
+     * 
+     * @throws InterruptedException
+     */
+    @Test
+    public void test_error_in_callback() throws InterruptedException {
+
+        final SequentialOperationScheduler scheduler = new SequentialOperationScheduler(ConcurrencyJre.create());
+
+        try {
+            Async.waitFor(new Operation<Success>() {
+
+                @Override
+                public void apply(final ValueCallback<Success> callback) {
+
+                    scheduler.schedule(new Operation<Success>() {
+
+                        @Override
+                        public void apply(final ValueCallback<Success> callback) {
+                            throw new RuntimeException("Error");
+                        }
+
+                    }, AsyncCommon.embed(callback, new Closure<Success>() {
+
+                        @Override
+                        public void apply(final Success o) {
+                            callback.onSuccess(o);
+                            throw new RuntimeException("Expected Error!");
+
+                        }
+
+                    }));
+
+                }
+
+            });
+
+            Assert.fail("Exception was expected.");
+        } catch (final Throwable t) {
+            // as expected
+        }
+
+        Thread.sleep(50);
 
         Async.waitFor(new Operation<Success>() {
 
